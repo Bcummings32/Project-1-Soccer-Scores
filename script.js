7d9213a0298bb0fca0531f3f727b272daa55b857
Pdnarmi <pdnarmi@ncsu.edu>
2020-10-15 19:06:16 -0400

    made a few changes to html and js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 //functionality
 //sportsdb URL
-$document.ready(function() {
-    $(".input").blur(function () {
-        localStorage.setItem("Teams", $(this).val());
-        console.log("click");
-      }
-}
-    $.ajax({
-        type: 'GET',
-        url:
-        success: function(orders) {
-            $.each
-        }
-    })
-}
-    $('#submitPlayerName').on('click', function searchNames(e){
+// document.ready(function() {
+//     $(".input").blur(function () {
+//         localStorage.setItem("Teams", $(this).val());
+//         console.log("click");
+//       });
+// },
+    // $.ajax({
+    //     type: 'GET',
+    //     url:
+    //     success, function(orders) {
+    //         $.each
+    //     }
+    // }),
+$('#submitPlayerName').on('click', function searchNames(e){
         e.preventDefault();
         var playerFirstName = $('#playerFirstName').val();
         var playerLastName = $('#playerLastName').val();
@@ -29,7 +28,7 @@ $document.ready(function() {
             console.log(playerLastName);
         })
         }),
-    $('#stats').on('click', function searchTeamName(t){
+$('#stats').on('click', function searchTeamName(t){
         t.preventDefault();
         var teamName = $('#comment').val();
         var queryURL= "https://www.thesportsdb.com/api/v1/json/4013017/searchteams.php?t="+teamName;
@@ -41,9 +40,9 @@ $document.ready(function() {
             console.log(response);
             console.log(teamName);
         })
-        })
+        }),
         //AJAX for search teams
-        $('#search-teams').on('click', function searchTeamName(e){
+ $('#search-teams').on('click', function searchTeamName(e){
             e.preventDefault();
                var teamName = $('#teamName').val();
                var queryURL= "https://www.thesportsdb.com/api/v1/json/4013017/searchteams.php?t="+teamName;
@@ -57,7 +56,7 @@ $document.ready(function() {
            })
            });
             //ajax for search players
-           $('#search-players').on('click', function searchNames(e){
+$('#search-players').on('click', function searchNames(e){
             e.preventDefault();
                 var playerFirstName = $('#playerFirstName').val();
                 var playerLastName = $('#playerLastName').val();
@@ -71,34 +70,17 @@ $document.ready(function() {
                     console.log(playerFirstName);
                     console.log(playerLastName);
                 });
-                //ajax for upcoming events
-                $.ajax({
-                    url: "https://www.thesportsdb.com/api/v1/json/4013017/eventsnextleague.php?id=4328",
-                    method: "GET"
-                }).then(function(response){
-                    console.log(response);
-                });
-                //AJAX for event highlights
-                $.ajax({
-                    url: "https://www.thesportsdb.com/api/v1/json/4013017/eventshighlights.php?s=Soccer",
-                    method: "GET"
-                }).then(function(response){
-                    console.log(response);
-                });
-                $.ajax({
-                    url: "https://www.thesportsdb.com/api/v1/json/4013017/eventshighlights.php?s=Soccer",
-                    method: "GET"
-                }).then(function(response){
-                    console.log(response);
-                });
-
-
-
-
-
-
-
-
-
-
-
+ //ajax for upcoming events
+$.ajax({
+    url: "https://www.thesportsdb.com/api/v1/json/4013017/eventsnextleague.php?id=4328",
+    method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
+ //AJAX for event highlights
+ $.ajax({
+    url: "https://www.thesportsdb.com/api/v1/json/4013017/eventshighlights.php?s=Soccer",
+    method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })})
